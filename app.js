6cc1600f9489109e486c5111f21548b9e586a2e5
John Doe <shahiezasauki15@gmail.com>
2023-09-29 11:54:09 +0700

    baru

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const pointTable = document.getElementById("gisTable").getElementsByTagName('tbody')[0];
+    const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
 
-    fetch("data.json") // Ganti "data.json" dengan nama file JSON Anda
+    fetch("poin.json") // Ganti "data.json" dengan nama file JSON Anda
         .then(response => response.json())
         .then(data => {
             data.features.forEach(feature => {
+                if (feature.geometry.type === "Point") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.titik;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("polygonTable").getElementsByTagName('tbody')[0];
+
+    fetch("polygon.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "Polygon") {
                     const row = pointTable.insertRow();
                     const nameCell = row.insertCell(0);
                     const coordinatesCell = row.insertCell(1);
@@ -12,6 +35,30 @@ document.addEventListener("DOMContentLoaded", () => {
                     nameCell.innerText = feature.properties.tempat;
                     coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
                     typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("polylineTable").getElementsByTagName('tbody')[0];
+
+    fetch("polyline.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "LineString") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.jalan;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
             });
         })
         .catch(error => console.error("Terjadi kesalahan:", error));
